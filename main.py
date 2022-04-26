a49ebf53e894b39c2a50e3141247272ef5185fec
Daniil Sergejev <77155713+DaniilsSergejevs@users.noreply.github.com>
2022-04-26 08:58:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+a = int(input('Ievadi brāļu vecumu: '))
+b = int(input('Ievadi māsu vecumu: '))
+if a>b:
+  print("Brāļis ir vecāks neka māsa")
+elif a==b:
+  print("Viņi ir dvīņi")
+else:
+  print("Māsa ir vecāka neka brāļis")
